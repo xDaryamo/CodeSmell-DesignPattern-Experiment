6c675cc7339b8588cffd6d18c6e3c82d70e1f21b
XJustUnluckyX <31347647+XJustUnluckyX@users.noreply.github.com>
2023-06-07 23:30:25 +0200

    Added BoxPlot script and dataset

---
+++ BoxPlot.R
@@ -0,0 +1,40 @@
+library(xlsx)
+
+dataA <- read.xlsx("D:\\Test\\Results.xlsx", sheetIndex = 2, rowIndex = 1:9)
+dataB <- read.xlsx("D:\\Test\\Results.xlsx", sheetIndex = 2, rowIndex = 10:18)
+dataC <- read.xlsx("D:\\Test\\Results.xlsx", sheetIndex = 2, rowIndex = 19:27)
+dataD <- read.xlsx("D:\\Test\\Results.xlsx", sheetIndex = 2, rowIndex = 28:36)
+
+xVector <- c("A", "B", "C", "D")
+
+dfA <- data.frame(xVector[1], dataA$Corrette.Comprensione)
+dfB <- data.frame(xVector[2], dataB$Corrette.Comprensione)
+dfC <- data.frame(xVector[3], dataC$Corrette.Comprensione)
+dfD <- data.frame(xVector[4], dataD$Corrette.Comprensione)
+
+statsA <- summary(dfA)
+statsB <- summary(dfB)
+statsC <- summary(dfC)
+statsD <- summary(dfD)
+
+library(ggplot2)
+ggplot(dfA, aes(x = xVector[1], y = dataA.Corrette.Comprensione)) +
+geom_boxplot() +
+geom_boxplot(data = dfB, aes(x = xVector[2], y = dataB.Corrette.Comprensione)) +
+geom_boxplot(data = dfC, aes(x = xVector[3], y = dataC.Corrette.Comprensione)) +
+geom_boxplot(data = dfD, aes(x = xVector[4], y = dataD.Corrette.Comprensione)) +
+labs(title = "Comprensione", x = "Group", y = "Risposte Corrette")
+
+dfA <- data.frame(xVector[1], dataA$Corrette.Manutenzione)
+dfB <- data.frame(xVector[2], dataB$Corrette.Manutenzione)
+dfC <- data.frame(xVector[3], dataC$Corrette.Manutenzione)
+dfD <- data.frame(xVector[4], dataD$Corrette.Manutenzione)
+
+library(ggplot2)
+ggplot(dfA, aes(x = xVector[1], y = dataA.Corrette.Manutenzione)) +
+  geom_boxplot() +
+  geom_boxplot(data = dfB, aes(x = xVector[2], y = dataB.Corrette.Manutenzione)) +
+  geom_boxplot(data = dfC, aes(x = xVector[3], y = dataC.Corrette.Manutenzione)) +
+  geom_boxplot(data = dfD, aes(x = xVector[4], y = dataD.Corrette.Manutenzione)) +
+  labs(title = "Manutenzione", x = "Group", y = "Risposte Corrette")
+
